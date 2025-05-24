d60278e65061afc0b3888f697d743e9ec5b532b8
Wasim Akram <wasimkram.syed@outlook.comgit config --global user.email wasimkram.syed@outlook.comgit config --global user.email wasimkram.syed@outlook.com>
2025-05-24 17:36:47 +0530

    Hello world java Code

---
+++ helloworld.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class helloworld {
+
+	public helloworld() {
+		System.out.println("helloworld");
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
